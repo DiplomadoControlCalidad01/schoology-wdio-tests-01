606cb606013f27f2e748b72a3e60b81cd070a1bc
Cristelh Miranda <cristelh.miranda@gmail.com>
2019-01-24 20:31:42 -0400

    Added basic tests

--- e2e/core/commonActions.js
+++ e2e/core/commonActions.js
@@ -1,16 +1,18 @@
+const env = require('../../environment');
+
 class CommonActions{
     static setValue(locator, value) {
-        browser.waitForVisible(locator, 30000);
+        browser.waitForVisible(locator, env.timeout.explicit);
         browser.element(locator).setValue(value);
     }
 
     static click(locator) {
-        browser.waitForVisible(locator, 30000);
+        browser.waitForVisible(locator, env.timeout.explicit);
         browser.element(locator).click();
     }
 
     static waitForVisible(locator) {
-        browser.waitForVisible(locator, 30000)
+        browser.waitForVisible(locator, env.timeout.explicit)
     }
 }
 module.exports = CommonActions;
